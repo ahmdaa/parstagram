d793def0f96acb308eaad96e94b1985f28810bd1
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-03-04 14:55:41 -0500

    Implement camera launch or image picking

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Ahmed Abdalla on 3/4/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var previewImageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        previewImageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
